0ad54212968d380048c63ae13e5c2ea18ad43b28
LozEsteban <a01197975@tec.mx>
2024-08-14 17:55:45 -0600

    Adelanto de un main, solo tiene 2 inputs

---
+++ main.py
@@ -0,0 +1,11 @@
+# Interfaz:
+print("Seleccione una operación:")
+print("1. Sumar")
+print("2. Restar")
+print("3. Multiplicar")
+print("4. Dividir")
+
+operacion = input("Ingresa el número de la operación deseada (1/2/3/4): ")
+
+numero1 = float(input("Ingresa el primer número: "))
+numero2 = float(input("Ingresa el segundo número: "))
