3de9278dfeb97713d578175785659b747da96e54
LozEsteban <a01197975@tec.mx>
2024-08-14 19:07:33 -0600

    Se agrega ejecución

--- main.py
+++ main.py
@@ -15,3 +15,22 @@ operacion = input("Ingresa el número de la operación deseada (1/2/3/4): ")
 numero1 = float(input("Ingresa el primer número: "))
 numero2 = float(input("Ingresa el segundo número: "))
 
+# Ejecución
+if operacion == '1':
+    print(f"{numero1} + {numero2} = {sumar(numero1, numero2)}")
+
+elif operacion == '2':
+    print(f"{numero1} - {numero2} = {restar(numero1, numero2)}")
+
+elif operacion == '3':
+    print(f"{numero1} * {numero2} = {multiplicar(numero1, numero2)}")
+
+elif operacion == '4':
+    resultado = dividir(numero1, numero2)
+    if resultado != "Error: División por cero":
+        print(f"{numero1} / {numero2} = {resultado}")
+    else:
+        print(resultado)
+
+else:
+    print("Operación no válida")
